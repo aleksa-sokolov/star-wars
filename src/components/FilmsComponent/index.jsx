0724534b30c3a3d08286d6fa0ca02d03e4cbca0a
unknown <you@example.com>
2022-09-29 12:55:01 +0400

    first commit

---
+++ src/components/FilmsComponent/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {GetApiStarWars} from "../../utils/getApiStarWars";
+
+const Films = () => {
+    GetApiStarWars("http https://swapi.dev/api/films/");
+    return (
+        <div className="films">
+           <div className="film">
+gfgfg
+           </div>
+        </div>
+    );
+};
+
+export default Films;
---
+++ src/pages/characters.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {useMediaQuery} from "usehooks-ts";
+
+const Peoples = () => {
+    // const matches = useMediaQuery('(min-width: 768px)');
+    return (
+        <>
+            Peoples
+            {/*{matches ? (<div>Peoples</div>) : (<div>Peoples 2</div>)}*/}
+        </>
+    );
+};
+
+export default Peoples;
